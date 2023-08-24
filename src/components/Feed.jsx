377bbad578fc9aac7f9ab3d5e8d1b882ea024485
Karl <toom.karl42@gmail.com>
2023-08-24 13:53:03 +0300

    started a youtube clone project

---
+++ src/components/Feed.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+function Feed() {
+  return <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}></Stack>;
+}
+
+export default Feed;
