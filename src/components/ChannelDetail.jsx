d55ae614ce9757dd7b416f251ccfc666723eb86b
Karl <toom.karl42@gmail.com>
2023-08-24 21:51:20 +0300

    added channel page and search function

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 function ChannelDetail() {
+  const { id } = useParams();
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  console.log(channelDetail, videos);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
   return (
-    <div>ChannelDetail</div>
-  )
+    <Box minHeight={"95vh"}>
+      <Box>
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(80,9,121,1) 0%, rgba(23,53,71,1) 56%, rgba(150,159,55,1) 100%)",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box display="flex" p='2'>
+        <Box sx={{mr: {sm:'100px'}}}/>
+          <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
 }
 
-export default ChannelDetail
+export default ChannelDetail;
